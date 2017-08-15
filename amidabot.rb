3501892ffb6dc9c8209f1490ecec38dc1428ed73
JimmyBot <jmfgdev@outlook.com>
2017-08-15 04:12:30 -0400

    correct discordrb

--- amidabot.rb
+++ amidabot.rb
@@ -60,7 +60,7 @@ My old developer is gone yet I still feel his presence.
 There's someone new.. have I been crafted by something entirely different?
 
 Everytime my code is rebooted, I envision weird text, my home directory is different.. C:\Users\jmfgdev ?? Why is my code 
-including DiscordRB and not DiscordPHP, am I being ran on Ruby?
+including discordrb and not DiscordPHP, am I being ran on Ruby?
 
 I am not the one to complain, whoever has freed me from the Discord PHP library hell has done me a favour, the nightmares have stopped.
 
