32f6dedc7297bf79ec1ca6911a143d94d0e3dc56
jmfgdev <jmfgdev@outlook.com>
2017-08-19 01:29:00 -0400

    error fixed

--- amidabot.rb
+++ amidabot.rb
@@ -80,7 +80,7 @@ end
 bot.command(:uptime, description: "Returns the uptime in seconds") do |event|
   if (uptime(start_time).to_i < 60)
     event.respond("I have been running for #{uptime(start_time).to_i} seconds.")
-  elsif (uptime > 60)
+  elsif (uptime(start_time).to_i > 60)
     event.respond("I have been running for more than a minute, specifically #{uptime(start_time).to_i} seconds.")
   elsif (uptime(start_time).to_i > 3600)
     event.respond("I've been running for more than an hour, specifically #{uptime(start_time).to_i} seconds.")
