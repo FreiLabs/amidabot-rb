9bf6b272c26ed52d64082d339fa8481d55f193ac
jmfgdev <jmfgdev@users.noreply.github.com>
2017-09-21 06:54:26 -0400

    Update run.rb

--- run.rb
+++ run.rb
@@ -14,6 +14,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>
 =end
 
+system "gem install discordrb"
 puts "Updating from git"
 system "git pull"
 puts "Updating system gems"
