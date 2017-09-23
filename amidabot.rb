c98dff0725c659412b4217e5aeaef08b918b76bc
jmfgdev <jmfgdev@users.noreply.github.com>
2017-09-23 07:26:19 +0000

    fix license headers

--- amidabot.rb
+++ amidabot.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 =begin
-Copyright © 2017 jimmybot@teknik.io
+Copyright (C) 2017 jmfgdev
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
@@ -125,7 +125,7 @@ bot.command(:whoami, description: "Make amidabot think to itself, who am i?") do
 end
 
 bot.command :license do |event|
-  event.respond("```Copyright © 2017 jmfgdev@outlook.com\nThis program is free software: you can redistribute it and/or modify\nit under the terms of the GNU Affero General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\nGNU Affero General Public License for more details.\nYou should have received a copy of the GNU Affero General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>```")
+  event.respond("```Copyright (C) 2017 jmfgdev\nThis program is free software: you can redistribute it and/or modify\nit under the terms of the GNU Affero General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\nGNU Affero General Public License for more details.\nYou should have received a copy of the GNU Affero General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>```")
 end
 
 bot.run
